9a19df0e91102d1093cbe9b6fa1ad9368d51b571
Samuel Davidson <samdamana@gmail.com>
2014-12-24 05:34:25 -0700

    Initial Commit
    Playing around with a plexiglass

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+namespace LoudMessaging
+{
+    class program
+    {
+            Application.EnableVisualStyles();
+            Application.Run(new RoutingForm());
+    }
+
+}
